3ecc8def97157dd46f217f13e3dc8271e4c22cdc
Abdullah Fareed <Abdullahfareed882@gmail.com>
2025-12-24 14:29:17 +0530

    added footer to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "../components/Footer";
 import EmpoweringSection from "./sections/EmpoweringSection";
 
 export default function About() {
   return (
     <main className="min-h-screen bg-white">
       <EmpoweringSection />
+      <Footer />
     </main>
   );
 }
