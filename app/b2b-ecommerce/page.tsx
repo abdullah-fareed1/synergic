71ad734a4b8f9b46018a21843999bb310d869c96
Abdullah Fareed <Abdullahfareed882@gmail.com>
2026-01-09 16:06:16 +0530

    B2B Ecom page: Completed the Design of the 2nd Section

--- app/b2b-ecommerce/page.tsx
+++ app/b2b-ecommerce/page.tsx
@@ -1,10 +1,11 @@
 import HeroSection from "./sections/HeroSection";
+import RetailerSolvingSection from "./sections/SolvingSection";
 
 export default function B2cSolutions() {
   return (
     <main className="min-h-screen">
         <HeroSection />
-        
+        <RetailerSolvingSection />
     </main>
   )
 }
