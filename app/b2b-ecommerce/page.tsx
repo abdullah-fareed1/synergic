9e8336f77d2daff5834470e8de8eb27da4108b4d
Abdullah Fareed <Abdullahfareed882@gmail.com>
2026-01-17 18:23:54 +0530

    B2B Ecommerce page: Added Technology Section

--- app/b2b-ecommerce/page.tsx
+++ app/b2b-ecommerce/page.tsx
@@ -1,12 +1,14 @@
 import Footer from "../components/Footer";
 import B2BCapabilitiesSection from "./sections/b2bCapabilitiesSection";
 import HeroSection from "./sections/HeroSection";
+import TechnologySection from "./sections/TechnologySection";
 
 export default function B2cSolutions() {
   return (
     <main className="min-h-screen">
         <HeroSection />
         <B2BCapabilitiesSection/>
+        <TechnologySection/>
         <Footer />
     </main>
   )
